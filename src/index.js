996f852dd0a2222b0c7d3f91624407659afbcb48
Aamir Pinger <aamirpinger@yahoo.com>
2019-10-07 15:36:14 +0500

    Firt basic node js app for jenkins

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("This is first basic node JS app with Jenkins")
+})
+
+app.listen(port, () => {
+    console.log("App is listening at http://localhost:" + port)
+})
