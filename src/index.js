4c98130212ed1fc9032d6ee89813145a816376d2
Aamir Pinger <aamirpinger@yahoo.com>
2019-10-18 12:34:57 +0500

    Changed in code to see the affect on browser

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send("This is first basic node JS app with Jenkins - first changed made")
+    res.send("This is first basic node JS app with Jenkins - second changed made")
 })
 
 app.listen(port, () => {
